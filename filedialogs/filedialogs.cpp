e49bb1a06cb7c604d7921a447ace9c798ba4bc90
high on tantor <josephvenable@verizon.net>
2026-01-04 19:43:28 -0500

    Update filedialogs.cpp

--- filedialogs/filedialogs.cpp
+++ filedialogs/filedialogs.cpp
@@ -284,6 +284,18 @@ namespace {
     if (ngs::fs::environment_get_variable("IMGUI_DIALOG_CANCELABLE").empty()) {
       ngs::fs::environment_set_variable("IMGUI_DIALOG_CANCELABLE", std::to_string(0));
     }
+    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_NOBORDER") != std::to_string(0)) {
+      ngs::fs::environment_set_variable("IMGUI_DIALOG_NOBORDER", std::to_string(1));
+    }
+    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_FULLSCREEN") != std::to_string(0)) {
+      ngs::fs::environment_set_variable("IMGUI_DIALOG_FULLSCREEN", std::to_string(1));
+    }
+    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_FULLSCREEN") == std::to_string(1)) {
+      ngs::fs::environment_unset_variable("IMGUI_DIALOG_PARENT");
+    }
+    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_CANCELABLE") != std::to_string(0)) {
+      ngs::fs::environment_set_variable("IMGUI_DIALOG_CANCELABLE", std::to_string(1));
+    }
     #if defined(SDL_VIDEO_DRIVER_X11)
     ngs::fs::environment_set_variable("SDL_VIDEODRIVER", "x11");
     #endif
@@ -392,15 +404,6 @@ namespace {
     };
     if (ngs::fs::environment_get_variable("IMGUI_FONT_LOADED") != std::to_string(0)) {
       ngs::fs::environment_set_variable("IMGUI_FONT_LOADED", std::to_string(1));
-    }
-    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_CANCELABLE") != std::to_string(0)) {
-      ngs::fs::environment_set_variable("IMGUI_DIALOG_CANCELABLE", std::to_string(1));
-    }
-    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_NOBORDER") != std::to_string(0)) {
-      ngs::fs::environment_set_variable("IMGUI_DIALOG_NOBORDER", std::to_string(1));
-    }
-    if (ngs::fs::environment_get_variable("IMGUI_DIALOG_FULLSCREEN") != std::to_string(0)) {
-      ngs::fs::environment_set_variable("IMGUI_DIALOG_FULLSCREEN", std::to_string(1));
     }
     ImVec4 clear_color = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
     string filterNew = imgui_filter(filter, (type == selectFolder));
